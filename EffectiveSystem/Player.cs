5a0e0b53298555e63dcb3d14690b22d1beea9fbd
Konrad Gadzina <konrad@makegames.today>
2024-05-21 13:42:58 +0200

    Use inheritance to introduce enemy class

--- EffectiveSystem/Player.cs
+++ EffectiveSystem/Player.cs
@@ -1,53 +1,20 @@
-class Player
+class Player : Character
 {
-    public string Visuals { get; set; }  = "@";
-    public int Hp
-    { 
-        get => hp;
-        set => hp = Math.Clamp(value, 0, MaxHp);
-    }
-    private int hp = 100;
-
-    public int MaxHp { get; private set; } = 100;
-
-    public Point Position { get; set; }
-    public Point PreviousPosition { get; set;}
-
     private Dictionary<ConsoleKey, Point> directions = new()
     {
         { ConsoleKey.A, new Point(-1, 0)},
         { ConsoleKey.D, new Point(1, 0)},
     };
 
-    public Player(int x, int y)
+    public Player(string visuals, Point startingPosition) : base(visuals, startingPosition)
     {
-        Position = new Point(x, y);
-        PreviousPosition = new Point(x, y);
-    }
-
-    public Player(Point startingPosition)
-    {
-        // Position = startingPosition;
-        // Position = new Point(startingPosition.X, startingPosition.Y);
-        Position = new Point(startingPosition);
-        PreviousPosition = new Point(startingPosition);
-        // directions.Add(ConsoleKey.A, new Point(-1, 0));
-        // directions.Add(ConsoleKey.A, new Point(1, 0));
-        // directions[ConsoleKey.A] = new Point(-1, 0);
-        // directions[ConsoleKey.A] = new Point(1, 0);
-        // directions[ConsoleKey.D] = new Point(1, 0);
         directions[ConsoleKey.W] = new Point(0, -1);
         directions[ConsoleKey.S] = new Point(0, 1);
         directions[ConsoleKey.E] = new Point(1, -1);
-        // directions.TryAdd
     }
 
-    public void Heal()
-    {
-        Hp += 10;
-    }
 
-    public Point GetNextPosition()
+    public override Point GetNextPosition()
     {
         Point nextPosition = new Point(Position);
         ConsoleKeyInfo pressedKey = Console.ReadKey(true);
@@ -60,44 +27,4 @@ class Player
 
         return nextPosition;
     }
-
-    public void Move(Point targetPosition)
-    {
-        PreviousPosition.X = Position.X;
-        PreviousPosition.Y = Position.Y;
-
-        Position.X = targetPosition.X;
-        Position.Y = targetPosition.Y;
-    }
 }
-
-
-/*
-Player
-    dane:
-    - hp
-    - maxhp
-    - mana
-    - maxmana
-    - int
-    - dex
-    - str
-    - inventory
-        - item
-        - item
-        - ...
-    - position
-        - x
-        - y
-
-    akcje:
-    - interact
-    - use item
-    - move
-    - open inventory
-    - take damage
-    - heal
-    - die/faint
-    - attack
-    - regenerate mana
-*/
--- EffectiveSystem/Program.cs
+++ EffectiveSystem/Program.cs
@@ -1,19 +1,46 @@
 ﻿Console.CursorVisible = false;
 Point playerPosition = new Point(10, 3);
-Player hero = new Player(playerPosition);
+
+Player hero = new Player("@", playerPosition);
+Player anotherHero = new Player("Q", new Point(1, 1));
+
+Enemy troll = new Enemy("T", new Point(4, 6));
+
 Map map = new Map();
 
 Console.Clear();
 map.Display(new Point(15, 2));
 
+map.DrawSomethingAt(hero.Visuals, hero.Position);
+map.DrawSomethingAt(anotherHero.Visuals, anotherHero.Position);
+map.DrawSomethingAt(troll.Visuals, troll.Position);
+
 while (true)
 {
-    map.RedrawCellAt(hero.PreviousPosition);
-    map.DrawSomethingAt(hero.Visuals, hero.Position);
-    
     Point nextPosition = hero.GetNextPosition();
     if (map.IsPointValid(nextPosition))
     {
         hero.Move(nextPosition);
+
+        map.RedrawCellAt(hero.PreviousPosition);
+        map.DrawSomethingAt(hero.Visuals, hero.Position);
+    }
+
+    Point anotherNextPosition = anotherHero.GetNextPosition();
+    if (map.IsPointValid(anotherNextPosition))
+    {
+        anotherHero.Move(anotherNextPosition);
+
+        map.RedrawCellAt(anotherHero.PreviousPosition);
+        map.DrawSomethingAt(anotherHero.Visuals, anotherHero.Position);
+    }
+
+    Point trollNextPosition = troll.GetNextPosition();
+    if (map.IsPointValid(trollNextPosition))
+    {
+        troll.Move(trollNextPosition);
+
+        map.RedrawCellAt(troll.PreviousPosition);
+        map.DrawSomethingAt(troll.Visuals, troll.Position);
     }
 }
